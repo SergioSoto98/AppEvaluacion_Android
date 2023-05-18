5aa73b25195f9d6138208eeb3cfb5b4848af3e9b
Sergio Soto <sersoto10@gmail.com>
2023-05-18 12:45:07 -0600

    Inicio de la App

---
+++ app/src/main/java/com/example/evaluaralumno/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluaralumno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
